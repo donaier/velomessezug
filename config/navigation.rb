7f6a312db985796625f7deee280feffc38ba560a
Donat Baier <donat.baier@gmail.com>
2014-02-19 17:03:32 +0100

    add home to nav

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,6 +3,7 @@ SimpleNavigation::Configuration.run do |navigation|
     navigation.selected_class = 'active'
 
     # build first level
+    primary.item 1, 'Home', '/'
     Kuhsaft::Page.find_by_slug_de('home').children.published.each do |page|
       unless page.url.blank?
         primary.item page.id, page.title, page.link, :highlights_on => %r(/#{page.title.downcase})
