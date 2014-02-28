41572c0b09ef91f98805b417c9d0cf7dfcfa0db4
Donat Baier <donat.baier@gmail.com>
2014-02-28 18:41:55 +0100

    update mailing

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,19 @@
 class ContactsController < ApplicationController
 
   def create
-    @name = params[:contact][:name]
-    @email = params[:contact][:email]
-    @message = params[:contact][:message]
 
-    if @name && @email && @message
+
+    if params[:contact][:name].present? && params[:contact][:email].present? && params[:contact][:message].present?
+
+      mail_subject = "Neue Kontaktaufnahme von #{params[:contact][:name]}"
+      mail_content = "#{params[:contact][:name]} (#{params[:contact][:email]}) schreibt:
+                      #{params[:contact][:message]}"
+
       Mail.deliver do
         to      'donat.baier@gmail.com'
         from    'kontaktformular@velomessezug.ch'
-        subject "Neue Kontaktaufnahme von #{@name}"
-        body    "#{@name} (#{@email}) schreibt:
-
-                #{@message}
-                "
+        subject mail_subject
+        body    mail_content
       end
       redirect_to contacts_thanks_path
     else
